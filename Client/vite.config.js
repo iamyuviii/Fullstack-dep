be8a915ea8636facaf9c02e8dad7c8a0f9669738
iamyuviii <bt22csh025@iiit.ac.in>
2025-04-15 15:36:21 +0530

    updated-day2-4

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // ← allows access from anywhere
+    port: 3000        // ← make sure it matches what's exposed in docker-compose
+  }
 })
